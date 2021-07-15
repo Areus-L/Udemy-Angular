9b85c3d4b49fca6f0e72588d10645c21546d3d51
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-07-15 10:22:02 +0200

    second assignment completed

--- section_2 - The Basics/src/app/app.module.ts
+++ section_2 - The Basics/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccesAlertComponent } from './succes-alert/succes-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning.component';
+import { AssignmentTwoComponent } from './assignment-two/assignment-two.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { WarningAlertComponent } from './warning-alert/warning.component';
     ServerComponent,
     ServersComponent,
     SuccesAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    AssignmentTwoComponent
   ],
   imports: [
     BrowserModule,
--- section_2 - The Basics/src/app/servers/servers.component.ts
+++ section_2 - The Basics/src/app/servers/servers.component.ts
@@ -22,7 +22,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = "Server was created!"
+    this.serverCreationStatus = "Server was created! Server name is " + this.serverName;
   }
 
   onUpdateServerName(event: any){
