140a9cede2e8f40f459f65d2587d5ac0d9751de1
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-07-15 11:06:39 +0200

    ngClass introduced

--- section_2 - The Basics/src/app/server/server.component.ts
+++ section_2 - The Basics/src/app/server/server.component.ts
@@ -2,13 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+      .online {
+        color: white;
+      }
+  `]
 })
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string  = 'online';
 
-  cunstructor(){
+  constructor(){
     this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
   }
   getServerStatus(){
