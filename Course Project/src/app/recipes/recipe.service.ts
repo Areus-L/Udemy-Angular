f7926adb87ba3663576cfd5d8af00cdead123d44
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-07-19 13:08:49 +0200

    Using a Service for Cross-Component Communication

--- Course Project/src/app/recipes/recipe.service.ts
+++ Course Project/src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
-  recipes: Recipe[] = [
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
     new Recipe('A test recipe', 'test', 'https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg'),
     new Recipe('Favorite meal', 'test', 'https://d3ur40406gizl1.cloudfront.net/web_site/images/v2/plates/4.png')
   ];
