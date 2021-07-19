2350ee01d240dffb2668922275d9ae7c38fb4d8a
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-07-19 12:58:17 +0200

    Managing Recipes in a Recipe Service

--- Course Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {Recipe} from '../recipe.model'
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,13 +9,12 @@ import {Recipe} from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'test', 'https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg'),
-    new Recipe('Favorite meal', 'test', 'https://d3ur40406gizl1.cloudfront.net/web_site/images/v2/plates/4.png')
-  ];
-  constructor() { }
+  recipes: Recipe[] = [];
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe){
--- Course Project/src/app/recipes/recipe.service.ts
+++ Course Project/src/app/recipes/recipe.service.ts
@@ -1,3 +1,12 @@
-export class RecipeService {
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'test', 'https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg'),
+    new Recipe('Favorite meal', 'test', 'https://d3ur40406gizl1.cloudfront.net/web_site/images/v2/plates/4.png')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
