46a08cdfb19cd321e85d42c8652e2faae2bac65c
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-07-19 14:36:01 +0200

    Passing Ingreds from Recipes to the Shopping List

--- Course Project/src/app/recipes/recipe.service.ts
+++ Course Project/src/app/recipes/recipe.service.ts
@@ -1,9 +1,13 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
+
+  constructor(private slService: ShoppingListService){}
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -26,5 +30,9 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addingredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
   }
 }
