4f0e7ddbfdf9a2da84fd38a25743d739acc24227
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-07-19 20:51:26 +0200

    Fetching Route Parameters Reactively

--- section_11 - Changing Pages with Routing/src/app/app.module.ts
+++ section_11 - Changing Pages with Routing/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent}, //localhost:4200/
-  { path: 'users/:id', component: UsersComponent}, //localhost:4200/users
+  { path: 'users/:id/:name', component: UserComponent}, //localhost:4200/user/1/Max
   { path: 'servers', component: ServersComponent} //localhost:4200/servers
 ]
 
