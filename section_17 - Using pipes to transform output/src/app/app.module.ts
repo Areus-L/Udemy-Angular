0f32b93a486e3546a9f8ea8fe116e756f0b1d451
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-07-22 20:34:47 +0200

    start pipes project

--- section_17 - Using pipes to transform output/src/app/app.module.ts
+++ section_17 - Using pipes to transform output/src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AssignmentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
