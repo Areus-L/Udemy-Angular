93adeaf508755c8325fae482e5425ff66dd534bb
Areus-L <87375411+Areus-L@users.noreply.github.com>
2021-07-22 21:28:33 +0200

    Creating a Filter Pipe

--- section_17 - Using pipes to transform output/src/app/app.module.ts
+++ section_17 - Using pipes to transform output/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ShortenPipePipe } from './shorten-pipe.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipePipe
+    ShortenPipePipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
